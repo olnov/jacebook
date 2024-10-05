fce1b3870cba6999d5ef8baa7d41e96cee4cf54e
Oleg Novikov <o.novikov@ymail.com>
2024-10-05 23:48:39 +0100

    Added ability to edit own post

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -6,6 +6,7 @@ import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -61,7 +62,7 @@ public class PostsController {
     }
 
     @DeleteMapping("/posts/{post_id}")
-    public String deletePost(@PathVariable("post_id") Long post_id, HttpSession session) {
+    public ResponseEntity<String> deletePost(@PathVariable("post_id") Long post_id, HttpSession session) {
         Long userId = (Long) session.getAttribute("user_id");
         if (userId == null) {
             throw new RuntimeException("Not authorized");
@@ -73,6 +74,21 @@ public class PostsController {
         } else {
             throw new RuntimeException("You are not allowed to delete this post");
         }
-        return "Deleted successfully";
+        return ResponseEntity.ok("Deleted successfully");
+    }
+
+    @PutMapping("/posts/{post_id}")
+    public ResponseEntity<String> editPost(@PathVariable("post_id") Long post_id, @RequestParam("content") String content, HttpSession session) {
+        Long userId = (Long) session.getAttribute("user_id");
+        if (userId == null){
+            throw new RuntimeException("Not authorized");
+        }
+        Post existingPost = postRepository.findById(post_id).
+                orElseThrow(()-> new RuntimeException("Post not found"));
+        if (Objects.equals(existingPost.getUser().getId(),userId)) {
+            existingPost.setContent(content);
+            postRepository.save(existingPost);
+        }
+        return ResponseEntity.ok("Post updated successfully");
     }
 }
