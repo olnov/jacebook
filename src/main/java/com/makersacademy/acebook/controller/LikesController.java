0d9406b62b6f188a65e97858be81ba88a7761c76
Harry <hmsh2000@gmail.com>
2024-10-07 10:36:50 +0100

    mains code w/ fridays likesController + PostsController

--- src/main/java/com/makersacademy/acebook/controller/LikesController.java
+++ src/main/java/com/makersacademy/acebook/controller/LikesController.java
@@ -1,19 +1,71 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Like;
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.LikeRepository;
+import com.makersacademy.acebook.repository.PostRepository;
+import com.makersacademy.acebook.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Map;
+
+@Controller
 public class LikesController {
 
     @Autowired
     LikeRepository likeRepository;
 
+    @Autowired
+    UserRepository userRepository;
 
+    @Autowired
+    PostRepository postRepository;
+
+    @PostMapping("/likes")
+    public RedirectView createLike(@RequestParam("post_id") Long post_id, HttpSession session) {
+        System.out.println("THE FRONT END HAS MADE THE REQUEST");
+        Long userId = (Long) session.getAttribute("user_id");
+        userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        postRepository.findById(post_id)
+                .orElseThrow(() -> new RuntimeException("Post not found"));
+        Like like = new Like(post_id, userId);
+
+        likeRepository.save(like);
+        return new RedirectView("/");
+    }
+
+    @DeleteMapping("/likes-delete")
+    public RedirectView deleteLike(@RequestParam("post_id") Long post_id, HttpSession session) {
+        Long userId = (Long) session.getAttribute("user_id");
+        Like like = likeRepository.findByUserIdAndPostId(userId, post_id)
+                .orElseThrow(() -> new RuntimeException("like not found"));
+        likeRepository.delete(like);
+        return new RedirectView("/");
+    }
+
+
+    @PostMapping("/likes")
+    public RedirectView createLike(@RequestBody Map<String, Long> payload, HttpSession session) {
+
+        System.out.println("THE FRONT END HAS MADE THE REQUEST");
+        Long post_id = payload.get("post_id");
+        Long userId = (Long) session.getAttribute("user_id");
+        userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        postRepository.findById(post_id)
+                .orElseThrow(() -> new RuntimeException("Post not found"));
+
+        Like like = new Like(userId, post_id);
+        likeRepository.save(like);
+
+        return new RedirectView("/");
+    }
 }
-
-// Controller
-// Post mapping - When user clicks 'like', post mapping posts to likes table and adds user_id to respective post_id
-// get mapping - Retrieving how many user_ids are related to that post
-//
--- src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
@@ -3,8 +3,11 @@ package com.makersacademy.acebook.repository;
 import com.makersacademy.acebook.model.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
+    Optional<Like> findByUserIdAndPostId(Long user_id, Long post_id);
 }
 
 
